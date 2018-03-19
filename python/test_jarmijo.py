8dabeb04549199a5cee121a9834100a31d0a561f
bartok10 <jnarmijo@uc.cl>
2018-03-19 12:42:51 -0300

    replace test_jarmijo.py

--- python/test_jarmijo.py
+++ python/test_jarmijo.py
@@ -23,11 +23,12 @@ adj_filename = dir1+"cat_zobov.ascii.adj"
 
 dim2 = ZobovTess([], gal, vol, zones, af, adj_filename)
 
-a = dim2.get_voronoi_cel_from_id(0)#recibe ID galaxia
+a = dim2.get_voronoi_cel_from_id(31)#recibe ID galaxia
 vert = np.array(a.vertices)
 ridges = np.array(a.ridge_vertices) #caras de un poliedro
 
-faces=[] #caras sin -1
+faces=[] #w/o -1
+cc = [] # geometric centres
 for ll in ridges:
     ct=0
     for c in ll:
@@ -35,6 +36,16 @@ for ll in ridges:
         ct +=1
     if ct == len(ll):
         faces.append(ll)
+	cc.append(np.mean(a.vertices[ll],axis=0))
+
+Nfaces = len(dim2.adj_table['ids_adj'][31]) # number of adj points
+cc = np.array(cc)
+dist = cc - a.points[0]
+l = np.linalg.norm(dist,axis=1)
+ID = np.arange(0,len(faces),1)
+l_sorted = np.sort(l)[:Nfaces]; ID_sorted = ID[np.argsort(l)][:Nfaces]
+c_sorted = cc[ID_sorted]
+cFaces = np.array(faces)[ID_sorted] #clean faces (Nfaces closer faces)
 
 #script para plottear celdas Voronoi (utils.py)
 f = plt.figure(figsize=(8,6))
@@ -50,13 +61,14 @@ ax.set_xlabel('X',fontsize=20)
 ax.set_ylabel('Y',fontsize=20)
 ax.set_zlabel('Z',fontsize=20)
 ax.plot([gal_pos[0]], [gal_pos[1]], [gal_pos[2]], 'ro',markersize=4*scale)
+ax.plot(c_sorted[:,0],c_sorted[:,1],c_sorted[:,2],'r*',markersize=12)
 vtx = [pos[0] for pos in a.vertices]
 vty = [pos[1] for pos in a.vertices]
 vtz = [pos[2] for pos in a.vertices]
 ax.plot(vtx, vty, vtz, '^g',markersize=4*scale)
-for i in range(len(faces)):
-    poli = Poly3DCollection([a.vertices[faces[i]]])
+for i in range(len(cFaces)):
+    poli = Poly3DCollection([a.vertices[cFaces[i]]])
     poli.set_edgecolor('k')
-    poli.set_facecolor('gray')
+    poli.set_facecolor('w')
     poli.set_alpha(alpha=0.1)
     ax.add_collection3d(poli)
